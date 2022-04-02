7f1a0c8b641621ca1ee9cc51f17ec9fbae748a3a
Geenstack <hayleytops@gmail.com>
2022-04-02 19:40:59 +0300

    Функция собирает все рефы по уязвимости через Vulners и сортирует их по типу бюллетеня

---
+++ vulners_module.py
@@ -0,0 +1,27 @@
+import vulners
+
+
+vulners_api = vulners.VulnersApi(api_key="INSERT YOUR KEY")
+
+
+def get_bulletins(cve):
+    result = {"metadata":{"bulletinFamily":{}}, "bulletins":{}}
+    vulners_response = vulners_api.get_multiple_bulletin_references(cve)
+    for i in vulners_response:
+        for bulletin in vulners_response[i]:
+            if bulletin["bulletinFamily"] not in result["metadata"]:
+                result["metadata"]["bulletinFamily"].update({bulletin["bulletinFamily"]:1})
+                result["bulletins"].update({bulletin["bulletinFamily"]:[]})
+            else:
+                result["metadata"]["bulletinFamily"][bulletin["bulletinFamily"]] += 1
+
+            result["bulletins"][bulletin["bulletinFamily"]].append(
+                {
+                    "id":bulletin["id"],
+                    "title":bulletin["title"],
+                    "type":bulletin["type"],
+                    "href":bulletin["href"]
+                }
+            )
+
+    return result
