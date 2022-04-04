a8a1a0085c1f97d74772974755e8f29ccbcaac6e
Geenstack <hayleytops@gmail.com>
2022-04-04 21:33:21 +0300

    Создание отчета

---
+++ main.py
@@ -0,0 +1,9 @@
+from vulners_module import *
+from render_vulners_bulletin import *
+
+
+def generate_bulletin(cve):
+    data = render_vulners_bulletin(get_bulletins(cve))
+    f = open("{}.html".format(cve), "wb")
+    f.write(data.encode())
+    f.close()
---
+++ render_vulners_bulletin.py
@@ -0,0 +1,6 @@
+from jinja2 import Environment, FileSystemLoader
+
+def render_bulletin(bulletin_data):
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template("vulners_report_template.html")
+    return template.render(data)
