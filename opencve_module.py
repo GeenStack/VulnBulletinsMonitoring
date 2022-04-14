9a7be3e8ec69f9791ab94eb74a8715b72e29213c
GeenStack <hayleytops@gmail.com>
2022-04-14 16:12:54 +0300

    Восстанавливаю модуль для OpenCVE

--- opencve_module.py
+++ opencve_module.py
@@ -1,3 +1,26 @@
 from config import OPENCVE_HEADER
 import requests
 
+def get_opencve_data(cve):
+    url = "https://www.opencve.io/api/cve/{}".format(cve)
+    headers = {"Authorization":OPENCVE_HEADER}
+    r = requests.get(url, headers=headers)
+    data = r.json()
+
+    summary = data["summary"]
+    cwe = data["cwes"]
+    cvss2 = data['raw_nvd_data']['impact']['baseMetricV2']
+    cvss3 = data['raw_nvd_data']['impact']['baseMetricV3']
+    nvd_tags = []
+    for ref in data['raw_nvd_data']['cve']['references']['reference_data']:
+        pass
+
+
+    '''
+        data['raw_nvd_data']['impact']
+    '''
+
+
+
+
+    return r.json()
